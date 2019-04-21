720bc8871206fec67ff8b0eddc81c8d90a0514e2
Eric Rowell <ericrowell@Erics-MacBook-Pro.local>
2019-04-21 00:25:29 -0700

    update dist files

--- build/concrete.js
+++ build/concrete.js
@@ -1,5 +1,5 @@
 /*
- * Concrete v3.0.3
+ * Concrete v3.0.4
  * A lightweight Html5 Canvas framework that enables hit detection, layering, multi buffering, 
  * pixel ratio management, exports, and image downloads
  * Release Date: 4-21-2019
